d5ce041945987ac1ab6873a6b3dee11173266f6c
Your Name <you@example.com>
2026-03-05 15:50:25 +0900

    feat: working on order.py

--- order.py
+++ order.py
@@ -1,3 +1,5 @@
+# Working #####################################################################
+
 def create_order(customer, item, discount=0):
     return f"[ORDER] {customer} ordered {item} with {discount}% discount"
 
