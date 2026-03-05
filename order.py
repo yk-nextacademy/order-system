8572a8d111688285419032b6a00e5166a6f9b9d2
Your Name <you@example.com>
2026-03-05 15:51:48 +0900

    Revert "feat: working on order.py"
    This reverts commit d5ce041945987ac1ab6873a6b3dee11173266f6c.

--- order.py
+++ order.py
@@ -1,5 +1,3 @@
-# Working #####################################################################
-
 def create_order(customer, item, discount=0):
     return f"[ORDER] {customer} ordered {item} with {discount}% discount"
 
